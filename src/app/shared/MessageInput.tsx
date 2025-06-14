fe2427870352d4d3604d7519e843d93cac78c16b
jldec <jurgen@jldec.me>
2025-06-14 09:58:17 +0100

    improve MessageInput props types

--- src/app/shared/MessageInput.tsx
+++ src/app/shared/MessageInput.tsx
@@ -1,17 +1,25 @@
 'use client'
 import { useState, useRef, useEffect } from 'react'
 
-// optional props added to support useChatAgent props in the agent sdk
-// must provide either newMessage or onSubmit + value + onChange 
 interface MessageInputProps {
-  newMessage?: (prompt: string) => Promise<void>
-  value?: string
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
-  onSubmit?: (event?: { preventDefault?: () => void }) => void
-  onClear: (() => Promise<void>) | (() => void)
+  newMessage: (prompt: string) => Promise<void>
+  onClear: () => Promise<void>
+  value?: never
+  onChange?: never
+  onSubmit?: never
 }
 
-export function MessageInput({ newMessage, value, onChange, onSubmit, onClear }: MessageInputProps) {
+interface MessageInputPropsAgentSDK {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onSubmit: (event?: { preventDefault?: () => void }) => void
+  onClear: () => void
+  newMessage?: never
+}
+
+type MessageInputPropsUnion = MessageInputProps | MessageInputPropsAgentSDK
+
+export function MessageInput(props: MessageInputPropsUnion) {
   const [input, setInput] = useState('')
   const inputRef = useRef<HTMLInputElement>(null)
 
@@ -31,20 +39,20 @@ export function MessageInput({ newMessage, value, onChange, onSubmit, onClear }:
     if (input.trim() === '') return
     const prompt = input
     setInput('') // TODO: disable input and keep showing prompt while waiting for response
-    newMessage ? await newMessage(prompt) : console.error('MessageInput is missing newMessage or onSubmit prop')
+    await props.newMessage!(prompt)
   }
 
   async function clear(e: React.FormEvent<HTMLButtonElement>) {
     e.preventDefault()
-    await onClear()
+    await props.onClear()
   }
   return (
-    <form onSubmit={onSubmit ?? submit} className="mt-2 mb-[10vh] flex flex-row gap-2">
+    <form onSubmit={'onSubmit' in props ? props.onSubmit : submit} className="mt-2 mb-[10vh] flex flex-row gap-2">
       <input
         ref={inputRef}
-        onChange={onChange ?? ((e) => setInput(e.target.value))}
+        onChange={'onChange' in props ? props.onChange : (e) => setInput(e.target.value)}
         className="flex-grow border-2 border-gray-400 p-2 w-full rounded-md"
-        value={value ?? input}
+        value={'value' in props ? props.value : input}
       />
       <button type="submit" className="p-2 bg-blue-500 text-white rounded-md cursor-pointer">
         Submit
