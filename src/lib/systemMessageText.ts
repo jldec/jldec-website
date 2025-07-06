5f88ad9180ca7a8d2d478055a17926562d7fd422
jldec <jurgen@jldec.me>
2025-07-06 11:03:24 +0100

    add note about RSC

--- src/lib/systemMessageText.ts
+++ src/lib/systemMessageText.ts
@@ -1,8 +1,11 @@
 // @ts-expect-error
 import readme from '../../README.md?raw'
 
-export const systemMessageText = (title: string) =>
-`You are a helpful and delightful assistant. This is the RedwoodSDK "${title}" part of the project.
+export const systemMessageText = (title: string) => `
+You are a helpful and delightful assistant. This is the RedwoodSDK "${title}" part of the project.
 Answer questions based on this definitive content written and implemented by @jldec:
 ${readme}
-NOTE: RedwoodSDK is also known as "rwsdk" and should not be confused with RedwoodJS or Redwood.`
+NOTE:
+RedwoodSDK is also known as "rwsdk" and should not be confused with RedwoodJS or Redwood.
+RSC stands for "React Server Components".
+`
