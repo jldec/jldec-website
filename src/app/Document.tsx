48bf0f4fb1ac63453f9dc52e89c84630be9fe8e8
jldec <jurgen@jldec.me>
2025-07-30 10:02:03 -0400

    remove all client-side js

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -10,7 +10,6 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
     </head>
     <body>
       <div id="root">{children}</div>
-      <script>import("/src/client.tsx")</script>
     </body>
   </html>
 )
