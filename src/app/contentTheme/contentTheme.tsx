169ae7bc02157160484de5cf5ca5a91ec26c8ea9
jldec <jurgen@jldec.me>
2025-08-04 14:06:16 +0100

    simplify 404

--- src/app/contentTheme/contentTheme.tsx
+++ src/app/contentTheme/contentTheme.tsx
@@ -1,30 +1,27 @@
-import { type RequestInfo, renderToStream } from 'rwsdk/worker'
-import { Document } from '@/app/Document'
+import { requestInfo } from 'rwsdk/worker'
 import { NotFound } from './404'
 import { Page } from './Page'
 import { Home } from './Home'
 import { BlogList } from './BlogList'
 import { BlogPost } from './BlogPost'
 
-export async function contentTheme({ ctx, request }: RequestInfo) {
-  if (ctx.pageContext?.pageData) {
-    switch (ctx.pageContext.pathname) {
+export async function contentTheme() {
+  const pageContext = requestInfo.ctx.pageContext
+  const pageData = pageContext?.pageData
+  if (pageData) {
+    switch (pageContext.pathname) {
       case '/':
         return <Home />
       case '/blog':
         return <BlogList />
       default:
-        if (ctx.pageContext.pathname.startsWith('/blog/')) {
+        if (pageContext.pathname.startsWith('/blog/')) {
           return <BlogPost />
         }
         return <Page />
     }
   } else {
-    // TODO: replace with requestInfo.status = 404 when available
-    // https://github.com/redwoodjs/sdk/issues/568
-    console.log(`404: ${request.url}`)
-    return new Response(await renderToStream(<NotFound />, { Document }), {
-      status: 404
-    })
+    requestInfo.response.status = 404
+    return <NotFound />
   }
 }
