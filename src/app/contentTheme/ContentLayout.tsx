6cd7a6db8784b62a4027e6a65eed8be93b27c7e5
jldec <jurgen@jldec.me>
2025-08-19 18:43:35 +0100

    bump deps, simplify layout

--- src/app/contentTheme/ContentLayout.tsx
+++ src/app/contentTheme/ContentLayout.tsx
@@ -1,8 +1,9 @@
+import { LayoutProps } from 'rwsdk/router'
 import { Menu } from './Menu'
 import { Metadata } from './Metadata'
 import { Splash } from './Splash'
 
-export function ContentLayout({ children }: { children: React.ReactNode }) {
+export function ContentLayout({ children }: LayoutProps) {
   return (
     <div className="max-w-3xl m-auto p-3">
       <Metadata />
