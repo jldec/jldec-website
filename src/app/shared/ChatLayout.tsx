e86c08879fd5bfeea3090e35d662643fc2b00919
jldec <jurgen@jldec.me>
2025-09-23 15:08:27 -0400

    deps, rm chat, rm image-plugin

--- src/app/shared/ChatLayout.tsx
+++
@@ -1,8 +0,0 @@
-export function ChatLayout({ title, children }: { title: string; children: React.ReactNode }) {
-  return (
-    <>
-      <h1 className="text-2xl font-bold my-2 px-2">{title}</h1>
-      {children}
-    </>
-  )
-}
