c20103bd5b82426e59147170d763faed3d99212f
jldec <jurgen@jldec.me>
2025-09-24 15:10:25 -0400

    slightly wider

--- src/app/contentTheme/ContentLayout.tsx
+++ src/app/contentTheme/ContentLayout.tsx
@@ -5,7 +5,7 @@ import { Splash } from './Splash'
 
 export function ContentLayout({ children }: LayoutProps) {
   return (
-    <div className="max-w-3xl m-auto p-3 overflow-hidden">
+    <div className="max-w-3xl m-auto py-3 px-3 lg:px-0 overflow-hidden">
       <Metadata />
       <Menu />
       <Splash />
