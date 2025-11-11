ab7ae06d31fd40da61f63ec3971de80d71e33c4b
jldec <jurgen@jldec.me>
2025-11-11 15:51:07 +0000

    turn on client-side js

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -10,6 +10,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
     </head>
     <body>
       <div id="root">{children}</div>
+      <script>import("/src/client.tsx")</script>
     </body>
   </html>
 )
