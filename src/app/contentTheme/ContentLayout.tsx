f484c58814d7580ec0f500ea606617011facb6bf
jldec <jurgen@jldec.me>
2026-01-03 14:58:24 -0600

    single ViewTransition instead of 2

--- src/app/contentTheme/ContentLayout.tsx
+++ src/app/contentTheme/ContentLayout.tsx
@@ -1,3 +1,4 @@
+import { ViewTransition } from 'react'
 import { LayoutProps } from 'rwsdk/router'
 import { Menu } from './Menu'
 import { Metadata } from './Metadata'
@@ -8,8 +9,10 @@ export function ContentLayout({ children }: LayoutProps) {
     <div className="max-w-3xl m-auto py-3 px-3 lg:px-0 overflow-hidden">
       <Metadata />
       <Menu />
-      <Splash />
-      {children}
+      <ViewTransition>
+        <Splash />
+        {children}
+      </ViewTransition>
       <script dangerouslySetInnerHTML={{ __html: 'import("/js/image-enlarge.js")' }} type="module"></script>
     </div>
   )
