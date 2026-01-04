92fb076110ee9415ab624d21c7b5e4afef874956
jldec <jurgen@jldec.me>
2026-01-04 14:20:38 -0600

    preload first 10 /blog splash images

--- src/app/contentTheme/BlogList.tsx
+++ src/app/contentTheme/BlogList.tsx
@@ -10,11 +10,13 @@ export function BlogList() {
       {pageData?.dir ? (
         <div className="prose max-w-none mt-4">
           <ul>
-            {pageData.dir.map((d) => {
+            {pageData.dir.map((d, i) => {
+              const preloadImage = i < 10 ? d.attrs?.splash?.image : null
               const text = d.attrs?.title ?? d.path
               const date = d.attrs?.date ?? ''
               return (
                 <li key={d.path} className="leading-[1.1rem] pb-1">
+                  {preloadImage ? <link rel="preload" href={preloadImage} as="image" /> : null}
                   <a
                     className="group flex items-end gap-4 no-underline border-b-[1.5px] border-b-transparent hover:border-b-current"
                     href={d.path}
